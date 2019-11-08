38ec711001f2d17cf1268883b794aadfa08cecca
maheshvarma27 <mahesh8.nadimpalli@gmail.com>
2019-11-08 13:18:32 +0530

    first commit

---
+++ usefullResourses.js
@@ -0,0 +1,19 @@
+//description: this project is used to convert excel files data to json data using file uploader multer
+
+
+// i got this code from the following website  on 8/11/2019
+// https://ciphertrick.com/read-excel-files-convert-json-node-js/
+
+// this project uses multer for file uploads.
+
+// 1) git clone https://github.com/rahil471/excel-to-json-in-Node.js.git
+// 2) cd excel-to-json-in-Node.js
+// 3) npm install
+// 4) node app.js
+// 5) In your browser http://localhost:3000
+// 6) Upload excel file and see result
+
+// note: to avoid deprecated warning use the latest packages from npm like 
+
+//      "xls-to-json-lc": "~0.3.4",
+//      "xlsx-to-json-lc": "~0.5.0"
